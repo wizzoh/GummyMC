6ce31574fe93e5fcd5877dbd435749687c2d659b
wizzoh <lollocoppa04@gmail.com>
2023-03-07 15:54:52 +0100

    add plugin

---
+++ src/main/java/me/wizzo/gummymc/GummyMC.java
@@ -0,0 +1,17 @@
+package me.wizzo.gummymc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GummyMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
